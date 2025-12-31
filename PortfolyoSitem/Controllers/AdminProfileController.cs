8ec378a651767ca3387d07a209c047f9866b8945
UgurNlbnt <ugur.nalbant617@gmail.com>
2025-12-31 12:29:35 +0300

    v0.3

--- PortfolyoSitem/Controllers/AdminProfileController.cs
+++ PortfolyoSitem/Controllers/AdminProfileController.cs
@@ -15,5 +15,43 @@ namespace PortfolyoSitem.Controllers
             var values = _context.ProfileTables.ToList();
             return View(values);
         }
+
+        [HttpGet]
+        public IActionResult CreateProfile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateProfile(ProfileTable profileTable)
+        {
+            _context.ProfileTables.Add(profileTable);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public IActionResult UpdateProfile(int id)
+        {
+            var values = _context.ProfileTables.Find(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateProfile(ProfileTable profileTable)
+        {
+            var values = _context.ProfileTables.Update(profileTable);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteProfile(int id)
+        {
+            var values = _context.ProfileTables.Find(id);
+            _context.ProfileTables.Remove(values);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
